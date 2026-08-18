61045b73771eb39f7295331a248b86653a47255b
João Victor Ribeiro <joao.vsantos2@gmail.com>
2026-08-18 09:00:14 -0300

    :sparkles: Criação de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
+import Cadastro from './pages/cadastro/Cadastro';
 import Home from './pages/home/Home';
+import Login from './pages/login/Login';
 
 function App() {
   return (
     <>
+    <BrowserRouter>
       <Navbar />
-      <Home />
+      <div className="min-h-[80vh]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      </div>
       <Footer />
+    </BrowserRouter>
     </>
   );
 }
